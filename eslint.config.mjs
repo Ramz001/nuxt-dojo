e3d8b6d4e2507c78d91bdb760325c6d5a4540cf7
Ramz001 <rkenjaev1@gmail.com>
2026-02-13 13:56:11 +0500

    chore: refactor eslint configuration for improved readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-  {
+export default withNuxt({
+  rules: {
     'vue/max-attributes-per-line': [
       'error',
       {
-        singleline: 1, // max 1 attribute per line on single-line elements
+        singleline: 1,
         multiline: {
-          max: 1, // max 1 per line on multi-line elements
+          max: 1,
           allowFirstLine: false
         }
       }
     ]
   }
-)
+})
